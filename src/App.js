d61d430b36f41c16ed649296b5bc852d8c2fb2a6
thisisnotdevin <devin1299@outlook.com>
2022-08-29 20:28:11 -0400

    added right-side

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+const themeToggler = document.querySelector(".theme-toggler");
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+themeToggler.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme-page');
+
+  themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+  themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+export default App;
