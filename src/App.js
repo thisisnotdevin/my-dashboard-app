84e5b3849637b9f34caca049cd9c41859c982cbf
thisisnotdevin <devin1299@outlook.com>
2022-08-31 21:53:32 -0400

    added extra info button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
 
 const themeToggler = document.querySelector(".theme-toggler");
 
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+//date format to set as today
+let today = new Date().toISOString().substr(0, 10);
+document.querySelector("#today").value = today;
+
 
 function App() {
   return (
